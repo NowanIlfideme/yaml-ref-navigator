28d693f4a90e9ea307c39170842f0a86fd1bc9ee
NowanIlfideme <git@nowan.dev>
2025-07-04 14:48:08 +0200

    Added actual YAML parsing.

--- src/extension.ts
+++ src/extension.ts
@@ -2,35 +2,54 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import * as yaml from 'yaml';  // npm install yaml
+import { parseDocument, LineCounter, isMap, isNode } from 'yaml';  // npm install yaml
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	console.log('✅ YAML Ref Navigator activated');
+function findMatchingKeys(obj: any, lineCounter: LineCounter): Map<string, vscode.Position> {
+	const result = new Map<string, vscode.Position>();
 
-	context.subscriptions.push(
-		vscode.languages.registerDefinitionProvider(
-			{ language: 'yaml', scheme: 'file' },
-			new ReferenceDefinitionProvider()
-		)
-	);
-}
+	function walk(node: any, path: string[] = []) {
+		if (isMap(node)) {
+			for (const item of node.items) {
+				const keyNode = item.key;
+				const valueNode = item.value;
 
-async function findYamlDefinitionInFile(refText: string, uri: vscode.Uri, content: string): Promise<vscode.Location[]> {
-	const results: vscode.Location[] = [];
-	const lines = content.split('\n');
+				if (!isNode(keyNode)) { continue; }
 
-	for (let i = 0; i < lines.length; i++) {
-		const line = lines[i];
-		if (line.trimStart().startsWith(`${refText}:`)) {
-			const position = new vscode.Position(i, line.indexOf(refText));
-			results.push(new vscode.Location(uri, position));
+				const key = keyNode.toString();
+				const newPath = [...path, key];
+				const fullPath = newPath.join('.');
+
+				// Get line/column using lineCounter
+				if (keyNode.range) {
+					const pos = lineCounter.linePos(keyNode.range[0]);
+					// NOTE: pos for yaml is 1-based, while vscode.Position is 0-based
+					result.set(fullPath, new vscode.Position(pos.line - 1, pos.col - 1));
+				}
+
+				walk(valueNode, newPath);
+			}
 		}
 	}
-	return results;
+
+	walk(obj.contents);
+	return result;
 }
 
+
+
+async function findYamlDefinitionInFile(refText: string, uri: vscode.Uri, content: string): Promise<vscode.Location[]> {
+	const lineCounter = new LineCounter();
+	const doc = parseDocument(content, { lineCounter });
+
+	const keyMap = findMatchingKeys(doc, lineCounter);
+	const position = keyMap.get(refText);
+
+	if (position) {
+		return [new vscode.Location(uri, position)];
+	}
+
+	return [];
+}
 
 async function findAllYamlDefinitions(refText: string): Promise<vscode.Location[]> {
 	const results: vscode.Location[] = [];
@@ -76,6 +95,19 @@ class ReferenceDefinitionProvider implements vscode.DefinitionProvider {
 		const locations = await findAllYamlDefinitions(refText);
 		return locations.length ? locations : null;
 	}
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('✅ YAML Ref Navigator activated');
+
+	context.subscriptions.push(
+		vscode.languages.registerDefinitionProvider(
+			{ language: 'yaml', scheme: 'file' },
+			new ReferenceDefinitionProvider()
+		)
+	);
 }
 
 // This method is called when your extension is deactivated
